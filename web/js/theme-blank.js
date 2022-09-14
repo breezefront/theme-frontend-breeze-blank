17da9aaf8f2ed809d8c813f4b84c705bf62148e3
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2022-09-14 16:51:01 +0300

    Fixed missing contact info in mobile slideout on non-breeze pages (cart)

---
+++ web/js/theme-blank.js
@@ -0,0 +1,14 @@
+define([
+    'jquery'
+], function ($) {
+    'use strict';
+
+    if (getComputedStyle(document.body).getPropertyValue('--header-panel-slideout')) {
+        $('.panel.header')
+            .clone()
+            .removeClass('header panel')
+            .addClass('mobile-header-panel')
+            .data('breeze-temporary', true)
+            .appendTo($('.navigation-wrapper'));
+    }
+});
